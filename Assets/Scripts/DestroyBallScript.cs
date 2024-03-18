3a81450f78bd9ecb5cc820dfbc7178dd8d2eaca5
LooneyTrooper66 <161621757+LooneyTrooper66@users.noreply.github.com>
2024-03-18 10:19:13 +0000

    rename

--- Assets/Scripts/DestroyBallScript.cs
+++ Assets/Scripts/DestroyBallScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallScript : MonoBehaviour
+public class DestroyBallScript : MonoBehaviour
 {
     public BallSpawnScript ballSpawn;
 
